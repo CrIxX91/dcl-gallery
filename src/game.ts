559cea1b04fa4a9b067593f576b5110f54b09705
Cristian Aguilar <cristian@motti.com>
2022-04-28 12:09:57 -0500

    Acomodo final y edificion nuevo

--- src/game.ts
+++ src/game.ts
@@ -14,7 +14,7 @@ const transform = new Transform({
 
 _scene.addComponentOrReplace(transform)
 // const gltfShape = new GLTFShape("Models/Stones1.glb")
-// const gtlfEdificio = new GLTFShape("Models/Edificio.glb")
+
 
 // const entity = new Entity('entity')
 // engine.addEntity(entity)
@@ -90,22 +90,23 @@ _scene.addComponentOrReplace(transform)
 
 /* Armado de escena Edificio y columnas */
 
-// const Edificio = new Entity('Edificio')
-// engine.addEntity(Edificio)
-// Edificio.setParent(_scene)
+// const gtlfEdificio = new GLTFShape("Models/edificio_7po.glb")
+const Edificio = new Entity('Edificio')
+engine.addEntity(Edificio)
+Edificio.setParent(_scene)
 
-// const transformEdificio = new Transform({
-//   position: new Vector3(16, 0, 24),
-//   rotation: new Quaternion(0, 0, 0, 1),
-//   scale: new Vector3(1, 1, 1)
-// })
-// Edificio.addComponentOrReplace(transformEdificio)
+const transformEdificio = new Transform({
+  position: new Vector3(16, 0, 24),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+Edificio.addComponentOrReplace(transformEdificio)
 
-// const gltfEdificio = new GLTFShape("Models/Edificio.glb")
-// gltfEdificio.withCollisions = true
-// gltfEdificio.isPointerBlocker = true
-// gltfEdificio.visible = true
-// Edificio.addComponentOrReplace(gltfEdificio)
+const gltfEdificio = new GLTFShape("Models/edificio_7po.glb")
+gltfEdificio.withCollisions = true
+gltfEdificio.isPointerBlocker = true
+gltfEdificio.visible = true
+Edificio.addComponentOrReplace(gltfEdificio)
 
 
 // const Columnas = new Entity('Columnas')
@@ -163,138 +164,160 @@ _scene.addComponentOrReplace(transform)
 // gltfColumna.visible = true
 // Columna.addComponentOrReplace(gltfColumna)
 
-let initposf1 = new Vector3(13, 0, 16);
-let materialact = new Material();
-
-let poscolumx = 13;
-let poscolumz = 12;
-let poscolumy = 2;
-
-let actmat =0;
 
 
-// const Bears01 = new Texture("images/atlas/testuv.png")
-// const MaterialBear01 = new Material();
-// MaterialBear01.albedoTexture = Bears01
-// materialact = MaterialBear01;
 
-const TexP1_01 = new Texture("images/atlas/P1_01.png")
+const Bears01 = new Texture("images/atlas/testuv.png")
+const MaterialBear01 = new Material();
+MaterialBear01.albedoTexture = Bears01
+
+
+const TexP0_01 = new Texture("images/atlas/p0-1.png")
+const MatP0_01 = new Material();
+MatP0_01.albedoTexture = TexP0_01;
+
+const TexP0_02 = new Texture("images/atlas/p0-2.png")
+const MatP0_02 = new Material();
+MatP0_02.albedoTexture = TexP0_02;
+
+const TexP1_01 = new Texture("images/atlas/p1-1.png")
 const MatP1_01 = new Material();
 MatP1_01.albedoTexture = TexP1_01;
 
-const TexP1_02 = new Texture("images/atlas/P1_02.png")
+const TexP1_02 = new Texture("images/atlas/p1-2.png")
 const MatP1_02 = new Material();
 MatP1_02.albedoTexture = TexP1_02;
 
-const TexP2_01 = new Texture("images/atlas/P2_01.png")
+const TexP2_01 = new Texture("images/atlas/p2-1.png")
 const MatP2_01 = new Material();
 MatP2_01.albedoTexture = TexP2_01;
 
-const TexP2_02 = new Texture("images/atlas/P2_02.png")
+const TexP2_02 = new Texture("images/atlas/p2-2.png")
 const MatP2_02 = new Material();
 MatP2_02.albedoTexture = TexP2_02;
 
-const TexP3_01 = new Texture("images/atlas/P3_01.png")
+const TexP3_01 = new Texture("images/atlas/p3-1.png")
 const MatP3_01 = new Material();
 MatP3_01.albedoTexture = TexP3_01;
 
-const TexP3_02 = new Texture("images/atlas/P3_02.png")
+const TexP3_02 = new Texture("images/atlas/p3-2.png")
 const MatP3_02 = new Material();
 MatP3_02.albedoTexture = TexP3_02;
 
-const TexP4_01 = new Texture("images/atlas/P4_01.png")
+const TexP4_01 = new Texture("images/atlas/p4-1.png")
 const MatP4_01 = new Material();
 MatP4_01.albedoTexture = TexP4_01;
 
-const TexP4_02 = new Texture("images/atlas/P5_02.png")
+const TexP4_02 = new Texture("images/atlas/p4-2.png")
 const MatP4_02 = new Material();
 MatP4_02.albedoTexture = TexP4_02;
 
-const TexP5_01 = new Texture("images/atlas/P5_01.png")
+const TexP5_01 = new Texture("images/atlas/p5-1.png")
 const MatP5_01 = new Material();
 MatP5_01.albedoTexture = TexP5_01;
 
-const TexP5_02 = new Texture("images/atlas/P5_02.png")
+const TexP5_02 = new Texture("images/atlas/p5-2.png")
 const MatP5_02 = new Material();
 MatP5_02.albedoTexture = TexP5_02;
 
 
-const TexP6_01 = new Texture("images/atlas/P6_01.png")
+const TexP6_01 = new Texture("images/atlas/p6-1.png")
 const MatP6_01 = new Material();
 MatP6_01.albedoTexture = TexP6_01;
 
-const TexP6_02 = new Texture("images/atlas/P6_02.png")
+const TexP6_02 = new Texture("images/atlas/p6-2.png")
 const MatP6_02 = new Material();
 MatP6_02.albedoTexture = TexP6_02;
 
-const TexP7_01 = new Texture("images/atlas/P7_01.png")
-const MatP7_01 = new Material();
-MatP7_01.albedoTexture = TexP7_01;
 
-const TexP7_02 = new Texture("images/atlas/P7_02.png")
-const MatP7_02 = new Material();
-MatP7_02.albedoTexture = TexP7_02;
+let positionact:Vector3;
+let materialact = new Material();
 
+// let poscolumx = 13;
+// let poscolumz = 12;
+// let poscolumy = 8;
 
-const TexP8_01 = new Texture("images/atlas/P8_01.png")
-const MatP8_01 = new Material();
-MatP8_01.albedoTexture = TexP8_01;
+let flooract = 0;
+// let poscolumz =10.5;
+// let poscolumy =12;
 
-const TexP8_02 = new Texture("images/atlas/P8_02.png")
-const MatP8_02 = new Material();
-MatP8_02.albedoTexture = TexP8_02;
+let positiony:number[] =[2,8,13,18,23,28,33]
+let positionsx:number[] = [11.5,11.5,11.5,11.5,11.5,11.5,11.5,11.5];
+let positionz:number[] =[13.5,17,17,17,17,17,17];
 
-const TexP9_01 = new Texture("images/atlas/P9_01.png")
-const MatP9_01 = new Material();
-MatP9_01.albedoTexture = TexP9_01;
-
-const TexP9_02 = new Texture("images/atlas/P9_02.png")
-const MatP9_02 = new Material();
-MatP9_02.albedoTexture = TexP9_02;
+//2
+//11.5
+//13.5
 
 
-// const TexTest = new Texture("images/atlas/testuv.png")
-// const MatTest = new Material();
-// MatTest.albedoTexture = TexTest;
-
-let positiony:number[] =[2,8,13,18,23,28,33,38,43]
+let poscolumx = positionsx[flooract];
+let poscolumz = positionz[flooract];
+let poscolumy = positiony[flooract];
+// let actx = positionsx[flooract];
 
 let Materials:Material[] =[
+  MatP0_01,MatP0_02,
   MatP1_01,MatP1_02,
   MatP2_01,MatP2_02,
   MatP3_01,MatP3_02,
   MatP4_01,MatP4_02,
   MatP5_01,MatP5_02,
   MatP6_01,MatP6_02,
-  MatP7_01,MatP7_02,
-  MatP8_01,MatP8_02,
-  MatP9_01,MatP9_02,
 ];
 
-materialact =Materials[0];
+materialact =Materials[flooract];
 
-for (var _i = 0; _i < 72; _i++) {
+for (var _i = 0; _i < 56; _i++) {
 
   if (_i !== 0) {
     if ((_i % 2) == 0) {
-
-      poscolumz += 8;
-      poscolumx = 13;
+      poscolumz += 7;
+      poscolumx = positionsx[flooract]
+      
     }
   }
 
   if (_i !== 0) {
     if ((_i % 8) == 0) {
-
-      poscolumx = 13;
-      poscolumz = 12;
-      actmat+=1;
-      materialact = Materials[actmat];
-      poscolumy = positiony[actmat];
+      flooract+=1;
+      poscolumy =positiony[flooract];
+      poscolumz = positionz[flooract];
+      materialact = Materials[flooract];
     }
   }
-  
-  initposf1 = new Vector3(poscolumx, poscolumy, poscolumz);
-  colum.CreateColum(initposf1, materialact);
-  poscolumx += 8;
+  positionact = new Vector3(poscolumx, poscolumy, poscolumz);
+  colum.CreateColum(positionact, materialact);
+  poscolumx += 9;
 }
+
+// materialact = MatP0_02;
+
+// for (var _i = 0; _i < 48; _i++) {
+
+//   if (_i !== 0) {
+//     if ((_i % 2) == 0) {
+
+//       poscolumz += 8;
+//       // actx = 13;
+//     }
+//   }
+
+//   if (_i !== 0) {
+//     if ((_i % 8) == 0) {
+
+//       // poscolumx = 13;
+//       flooract+=1;
+//       positionsx[flooract];
+//       materialact = Materials[flooract];
+//       poscolumy = positiony[flooract];
+//       // poscolumz = 12;
+//       // actmat+=1;
+//       // materialact = Materials[actmat];
+//       // poscolumy = positiony[actmat];
+//     }
+//   }
+  
+//   positionact = new Vector3(poscolumx, poscolumy, poscolumz);
+//   colum.CreateColum(initposf1, materialact);
+//   poscolumx += 8;
+// }
